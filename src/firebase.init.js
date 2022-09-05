7f12e23ce4831d176ac17d92116c4f283885c282
mdasif8393 <md.asif.8393@gmail.com>
2022-09-05 16:44:55 +0600

    add google sign in and sign out

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCjV7hJAGynyPEVXNVbeYuJmZxFCQNPA9w",
+  authDomain: "simple-firebase-authenti-b5.firebaseapp.com",
+  projectId: "simple-firebase-authenti-b5",
+  storageBucket: "simple-firebase-authenti-b5.appspot.com",
+  messagingSenderId: "859394212128",
+  appId: "1:859394212128:web:6cc81ab99dbf3ad2505745"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
